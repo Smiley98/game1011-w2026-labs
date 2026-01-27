c2b4ffd252e96b2d666686580ed52596bd69ab1f
Smiley98 <smileysofficialcinema@gmail.com>
2026-01-27 11:44:10 -0500

    Made LE1 starter

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <string>
+
+// 1) Make Platform, Game, and Achievement classes
+// (You'll want to create the Achievement class before the Game class, and create the Game class before the Platform class)
+
 int main()
 {
+	// 2) Make sure all data of all objects is initialized (each platform's name and manufacturer must be valid memory, not empty strings)
+	// Do the same for all games and all achievements.
+
+	// The data can be whatever you want, as long as the fields match that of the Lab Exercise 1 document!
 	return 0;
 }
