ac5bc74e66cb82bf9ca3fb3edd29046621b764ae
Smiley98 <smileysofficialcinema@gmail.com>
2026-02-03 11:35:46 -0500

    Encapsulated health

--- main.cpp
+++ main.cpp
@@ -1,26 +1,56 @@
 #include <iostream>
 #include <string>
 
-struct Player
+class Player
 {
-	int health;
+public:
+	std::string name;
+
+	static void SetBaseHealth(int base_health) { Player::base_health = base_health; }
+
 	void AddHealth(int health)
 	{
 		this->health = base_health + health;
 	}
 
+	int GetHealth() { return health; }
+
+private:
 	static int base_health;
+	int health;
 };
 
 int Player::base_health = 0;
 
+bool operator>(Player a, Player b)
+{
+	return a.GetHealth() > b.GetHealth();
+}
+
 int main()
 {
 	Player tank, assassin;
-	Player::base_health = 100;	// Initial health for all players
-	tank.AddHealth(150);		// Tank health = 100 base_health + 150 bonus health	
-	assassin.AddHealth(25);		// Assassin health = 100 base_health + 25 bonus health
+	tank.name = "Tank";
+	assassin.name = "Assassin";
+
+	int base_health, tank_bonus_health, assassin_bonus_health;
+
+	std::cout << "Please enter the base health of all players" << std::endl;
+	std::cin >> base_health;
+
+	std::cout << "Please enter the bonus health of " << tank.name << std::endl;
+	std::cin >> tank_bonus_health;
 	
+	std::cout << "Please enter the bonus health of " << assassin.name << std::endl;
+	std::cin >> assassin_bonus_health;
+
+	Player::SetBaseHealth(base_health);
+	tank.AddHealth(tank_bonus_health);
+	assassin.AddHealth(assassin_bonus_health);
+
+	std::cout << tank.name << " has " << tank.GetHealth() << " health." << std::endl;
+	std::cout << assassin.name << " has " << assassin.GetHealth() << " health." << std::endl;
+
 	return 0;
 }
 
