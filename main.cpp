250c8a54c256c0a398a980359faf5f9f0b6b34ac
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-10 11:44:12 -0400

    Added vec3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include "vec2.h" // User-defined header included with quotes ("") instead of angle brackets (<>)
+// User-defined header included with quotes ("") instead of angle brackets (<>)
+#include "vec2.h"
+#include "vec3.h"
+#include <iostream>
+
+void TestVec2();
+void TestVec3();
+void TestVec4();
 
 int main()
+{
+	TestVec2();
+	TestVec3();
+	//TestVec4();
+	return 0;
+}
+
+void TestVec2()
 {
 	{
 		vec2f a{ 1.0f, 2.0f };
@@ -9,17 +24,19 @@ int main()
 		vec2f d = a - b;
 		vec2f e = a * 2.0f;
 		vec2f f = a / 2.0f;
+		printf("vec2f\n");
 	}
 
 	{
-		vec2d a{ 1.0f, 2.0f };
-		vec2d b{ 3.0f, 4.0f };
+		vec2d a{ 1.0, 2.0 };
+		vec2d b{ 3.0, 4.0 };
 		vec2d c = a + b;
 		vec2d d = a - b;
-		vec2d e = a * 2.0f;
-		vec2d f = a / 2.0f;
+		vec2d e = a * 2.0;
+		vec2d f = a / 2.0;
+		printf("vec2d\n");
 	}
-	
+
 	// Note the difference in solutions for integers vs floats!
 	{
 		vec2i a{ 1, 2 };
@@ -28,7 +45,45 @@ int main()
 		vec2i d = a - b;
 		vec2i e = a * 2;
 		vec2i f = a / 2;
+		printf("vec2i\n");
+	}
+}
+
+void TestVec3()
+{
+	{
+		vec3f a{ 1.0f, 2.0f, 3.0f };
+		vec3f b{ 3.0f, 4.0f, 5.0f };
+		vec3f c = a + b;
+		vec3f d = a - b;
+		vec3f e = a * 2.0f;
+		vec3f f = a / 2.0f;
+		printf("vec3f\n");
 	}
 
-	return 0;
+	{
+		vec3d a{ 1.0, 2.0, 3.0 };
+		vec3d b{ 3.0, 4.0, 5.0 };
+		vec3d c = a + b;
+		vec3d d = a - b;
+		vec3d e = a * 2.0;
+		vec3d f = a / 2.0;
+		printf("vec3d\n");
+	}
+
+	// Note the difference in solutions for integers vs floats!
+	{
+		vec3i a{ 1, 2, 3 };
+		vec3i b{ 3, 4, 5 };
+		vec3i c = a + b;
+		vec3i d = a - b;
+		vec3i e = a * 2;
+		vec3i f = a / 2;
+		printf("vec3i\n");
+	}
+}
+
+void TestVec4()
+{
+
 }
--- vec3.h
+++ vec3.h
@@ -1,47 +1,52 @@
 #pragma once
 template<typename T>
-struct Vector2
+struct Vector3
 {
 	T x;
 	T y;
+	T z;
 };
 
 template<typename T>
-Vector2<T> operator+(Vector2<T> a, Vector2<T> b)
+Vector3<T> operator+(Vector3<T> a, Vector3<T> b)
 {
-	Vector2<T> v;
+	Vector3<T> v;
 	v.x = a.x + b.x;
 	v.y = a.y + b.y;
+	v.z = a.z + b.z;
 	return v;
 }
 
 template<typename T>
-Vector2<T> operator-(Vector2<T> a, Vector2<T> b)
+Vector3<T> operator-(Vector3<T> a, Vector3<T> b)
 {
-	Vector2<T> v;
+	Vector3<T> v;
 	v.x = a.x - b.x;
 	v.y = a.y - b.y;
+	v.z = a.z - b.z;
 	return v;
 }
 
 template<typename T>
-Vector2<T> operator*(Vector2<T> v, float s)
+Vector3<T> operator*(Vector3<T> v, float s)
 {
-	Vector2<T> r;
+	Vector3<T> r;
 	r.x = v.x * s;
 	r.y = v.y * s;
+	r.z = v.z * s;
 	return r;
 }
 
 template<typename T>
-Vector2<T> operator/(Vector2<T> v, float s)
+Vector3<T> operator/(Vector3<T> v, float s)
 {
-	Vector2<T> r;
+	Vector3<T> r;
 	r.x = v.x / s;
 	r.y = v.y / s;
+	r.z = v.z / s;
 	return r;
 }
 
-using vec2f = Vector2<float>;	// Compiler replaces "vec2f" with "Vector2<float>"
-using vec2d = Vector2<double>;	// Compiler replaces "vec2D" with "Vector2<double>"
-using vec2i = Vector2<int>;		// Compiler replaces "vec2I" with "Vector2<int>"
+using vec3f = Vector3<float>;	// Compiler replaces "vec2f" with "Vector2<float>"
+using vec3d = Vector3<double>;	// Compiler replaces "vec2D" with "Vector2<double>"
+using vec3i = Vector3<int>;		// Compiler replaces "vec2I" with "Vector2<int>"
