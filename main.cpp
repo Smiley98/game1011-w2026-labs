96b43f333dc003b218304a721e89ef4048cd106d
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-31 11:47:40 -0400

    Added time library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <Windows.h>
 #include <iostream>
 
+#pragma comment(lib, "Winmm.lib")
+
+enum Key
+{
+	KEY_0 = 0x30,
+	KEY_1,
+	KEY_2,
+	KEY_3,
+	KEY_4,
+	KEY_5,
+	KEY_6,
+	KEY_7,
+	KEY_8,
+	KEY_9,
+
+	KEY_A = 0x41,
+	KEY_B,
+	KEY_C,
+	KEY_D,
+	KEY_E,
+	KEY_F,
+	KEY_G,
+	KEY_H,
+	KEY_I,
+	KEY_J,
+	KEY_K,
+	KEY_L,
+	KEY_M,
+	KEY_N,
+	KEY_O,
+	KEY_P,
+	KEY_Q,
+	KEY_R,
+	KEY_S,
+	KEY_T,
+	KEY_U,
+	KEY_V,
+	KEY_W,
+	KEY_X,
+	KEY_Y,
+	KEY_Z
+};
+
 const int SCREEN_SIZE = 16;
 
 char world[SCREEN_SIZE][SCREEN_SIZE]
@@ -37,6 +80,9 @@ struct Entity
 
 int main()
 {
+	float player_time_current = 0.0f;
+	float player_time_total = 0.5f;
+
 	Entity player;
 	player.x = SCREEN_SIZE / 2;
 	player.y = SCREEN_SIZE / 2;
@@ -46,18 +92,32 @@ int main()
 	{
 		if (GetAsyncKeyState(VK_ESCAPE))
 			running = false;
-
-		for (int row = 0; row < SCREEN_SIZE; row++)
+		
+		// Update the player based on ticks rather than frames (so it doesn't fly off the screen)
+		if (player_time_current >= player_time_total)
 		{
-			for (int col = 0; col < SCREEN_SIZE; col++)
+			player_time_current = 0.0f;
+			if (GetAsyncKeyState(KEY_W))
 			{
-				std::cout << world[row][col];
+				player.y--;
 			}
-			std::cout << std::endl;
 		}
 
-		// TODO -- Render everything to buffer instead of moving the cursor every draw call to fix flickering
-		Draw('@', player.x, player.y);
+		//for (int row = 0; row < SCREEN_SIZE; row++)
+		//{
+		//	for (int col = 0; col < SCREEN_SIZE; col++)
+		//	{
+		//		std::cout << world[row][col];
+		//	}
+		//	std::cout << std::endl;
+		//}
+		//
+		//// TODO -- Render everything to buffer instead of moving the cursor every draw call to fix flickering
+		//Draw('@', player.x, player.y);
+		//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
+
+		DWORD milliseconds = timeGetTime();
+		std::cout << "Time: " << milliseconds / 1000 << std::endl;
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
 	}
 
